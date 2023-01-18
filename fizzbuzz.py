5d88b75e0f758b9a2407700b3d049cdd9ca8a38a
ilya sinitsyn <baner1337228@gmail.com>
2023-01-18 18:47:32 +0300

    Make a FizzBuzz script

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+def fizz_buzz(begin, end):
+    b = ''
+    if begin > end:
+        return ''
+
+    for i in range(begin, end + 1):
+
+        if i % 3 == 0 and i % 5 == 0:
+            b += 'FizzBuzz '
+        elif i % 3 == 0:
+            b += 'Fizz '
+        elif i % 5 == 0:
+            b += 'Buzz ' 
+        else:
+            b += f'{str(i)} '
+        
+    return b.strip()
+
+print(fizz_buzz(7, 12))
+
