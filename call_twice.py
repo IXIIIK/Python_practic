5feec06e803ea5f20d040b06b807043b7fead1a8
ilya sinitsyn <baner1337228@gmail.com>
2023-02-13 21:02:56 +0300

    make a HOF functions

--- call_twice.py
+++ call_twice.py
@@ -1,15 +1,7 @@
-def a(func):
-    def inner(args, **kwargs):
-        func(args, **kwargs)
-        res = func(args)
-        return res
-    return inner
-
 def call_twice(arg, *args, **kwargs):
 
     a = arg(*args, **kwargs), arg(*args, **kwargs)
     return a
 
-print(call_twice(input, 'enter: '))
 
 
